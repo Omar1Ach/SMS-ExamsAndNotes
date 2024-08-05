b64517c19c609af5ad4c9465c174b72882e0d7df
mohamedberraho43@gmail.com <144127085+Mohamedberraho@users.noreply.github.com>
2024-08-05 14:21:44 +0200

    Implemented new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SurveillantList from "./components/Surveillants/SurveillantList.jsx";
 import AddSurveillant from "./components/Surveillants/AddSurveillant.jsx";
 import UpdateSurveillant from "./components/Surveillants/UpdateSurveillant.jsx";
-import Header from './components/Header'
-import Sidebar from './components/SideBar'
+import Header from './components/Layouts/Header'
+import Sidebar from './components/Layouts/SideBar'
 
 
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Planification from "./Pages/PlanificationPage";
-import Sidebar from "./components/Layouts/SideBar";
-import Header from "./components/Layouts/Header";
->>>>>>> origin/main
 function App() {
-  return (
-<<<<<<< HEAD
+  const [count, setCount] = useState(0)
 
-=======
->>>>>>> origin/main
+  return (
+
     <Router>
       <div className="flex h-screen">
         <Sidebar />
         <div className="flex-1 flex flex-col">
           <Header />
-<<<<<<< HEAD
           <div className='p-10'>
             <Routes>
-              <Route path="/Home" element={<SurveillantList />} />
+              <Route path="/Surveillants" element={<SurveillantList />} />
               <Route path="/add-Surveillant" element={<AddSurveillant />} />
               <Route path="/Update/:id" element={<UpdateSurveillant />} />
             </Routes>
@@ -43,17 +34,6 @@ function App() {
 
 
   )
-=======
-          <div className="p-10">
-            <Routes>
-              <Route path="/Planification" element={<Planification />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
->>>>>>> origin/main
 }
 
-export default App;
+export default App
